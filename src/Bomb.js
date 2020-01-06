a20c0b20ba28e03959f003e9f38ea282e06f2b11
github username <github email address>
2020-01-06 12:21:08 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -11,7 +11,7 @@ export default class Bomb extends Component{
 
     render(){
         return(
-            this.state.secondsLeft === 0 ? <p>Boom!</p> : <p>120 seconds left before I go boom!</p>
+            this.state.secondsLeft === 0 ? <p>Boom!</p> : <p>{this.state.secondsLeft} seconds left before I go boom!</p>
         )
     }
 }
